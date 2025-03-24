9298391382b12959765e2d020d2c965dfa70d798
NaziraBatool <batoolnazira786@gmail.com>
2025-03-24 15:32:28 +0500

    pushing dropdown work

---
+++ dropdown/javascript assignment/script.js
@@ -0,0 +1,19 @@
+var num1=parseInt(prompt("enter first number"))
+var num2=parseInt(prompt("enter second number"))
+var result1=num1+num2;
+alert(result1)
+ 
+var num3=parseInt(prompt("enter first number"))
+var num4=parseInt(prompt("enter second number"))
+var result2=num3-num4;
+alert(result2);
+
+var num5=parseInt(prompt("enter first number"))
+var num6=parseInt(prompt("enter second number"))
+var result3=num5*num6;
+alert(result3);
+
+var num7=parseInt(prompt("enter first number"))
+var num8=parseInt(prompt("enter second number"))
+var result4=num7/num8;
+alert(result4);
